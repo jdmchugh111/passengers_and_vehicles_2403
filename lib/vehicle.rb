63eafee54e2cb73c903fa22a1c1390299ab57515
James McHugh <jdmchugh111@gmail.com>
2024-04-03 11:55:55 -0400

    Wrote tests for .passengers and passed them

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -18,4 +18,8 @@ class Vehicle
     def speed
         @speeding = true
     end
+
+    def add_passenger(passenger)
+        @passengers << passenger
+    end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -39,4 +39,23 @@ RSpec.describe Vehicle do
 
         expect(vehicle.speeding?).to eq(true)
     end
+
+    it "returns an empty array when no passengers have been added" do
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+        expect(vehicle.passengers).to eq([])
+    end
+
+    it "can add passengers" do
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+        jude = Passenger.new({"name" => "Jude", "age" => 20})   
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+        vehicle.add_passenger(charlie)
+        vehicle.add_passenger(jude)
+        vehicle.add_passenger(taylor)
+
+        expect(vehicle.passengers).to eq([charlie, jude, taylor])
+    end
 end
